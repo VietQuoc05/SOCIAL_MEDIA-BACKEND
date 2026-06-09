a9a63477d6bbc675fb9f2dc785b46556224a53d4
Nguyen Hoang Viet Quoc <ngquoc0112@gmail.com>
2026-06-09 10:57:34 +0700

    Comment2

--- src/modules/comments/comments.module.ts
+++ src/modules/comments/comments.module.ts
@@ -3,16 +3,26 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { Comment } from '../../database/entities/comment.entity';
 import { Post } from '../../database/entities/post.entity';
+import { Reaction } from '../../database/entities/reaction.entity'; // ✅ ADD
 
 import { CommentsService } from './comments.service';
 import { CommentsController } from './comments.controller';
 
+import { EventsGateway } from '../../events/events.gateway'; // ✅ ADD
+
 @Module({
   imports: [
-    TypeOrmModule.forFeature([Comment, Post]),
+    TypeOrmModule.forFeature([
+      Comment,
+      Post,
+      Reaction, // ✅ QUAN TRỌNG
+    ]),
   ],
-  providers: [CommentsService],
+  providers: [
+    CommentsService,
+    EventsGateway, // ✅ QUAN TRỌNG
+  ],
   controllers: [CommentsController],
+  exports: [CommentsService],
 })
 export class CommentsModule {}
-``

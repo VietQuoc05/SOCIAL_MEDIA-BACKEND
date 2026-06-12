c9ec53b86a1dfbb2cf0ff47fdf3b8676c3866b07
Nguyen Hoang Viet Quoc <ngquoc0112@gmail.com>
2026-06-12 13:59:11 +0700

    fix: add UploadController to UploadModule

--- src/modules/uploads/upload.module.ts
+++ src/modules/uploads/upload.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
+import { UploadController } from './upload.controller';
 import { UploadService } from './upload.service';
 import { S3Service } from './s3.service';
 
 @Module({
+  controllers: [UploadController],
   providers: [UploadService, S3Service],
   exports: [UploadService, S3Service],
 })

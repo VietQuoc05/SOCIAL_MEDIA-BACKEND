880940665eb7f3735c054f05d70c934e3e0569f9
Nguyen Hoang Viet Quoc <ngquoc0112@gmail.com>
2026-07-21 10:58:00 +0700

    fix deploy

--- src/modules/posts/posts.module.ts
+++ src/modules/posts/posts.module.ts
@@ -4,12 +4,14 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Post } from '../../database/entities/post.entity';
 import { Follow } from '../../database/entities/follow.entity';
 import { Reaction } from '../../database/entities/reaction.entity';
+import { User } from '../../database/entities/user.entity';
 
 import { PostsService } from './posts.service';
 import { PostsController } from './posts.controller';
 
 import { UploadModule } from '../uploads/upload.module';
 import { EventsModule } from '../../events/events.module';
+import { UsersModule } from '../users/users.module';
 
 @Module({
   imports: [
@@ -17,9 +19,11 @@ import { EventsModule } from '../../events/events.module';
       Post,
       Follow,
       Reaction,
+      User,
     ]),
     UploadModule,
     EventsModule,
+    UsersModule,
   ],
   providers: [PostsService],
   controllers: [PostsController],
